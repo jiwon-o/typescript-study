58b72c1308ff21c8ffa9361035e4e22a954bb7a2
Jiwon Oh <64193469+jiwon-o@users.noreply.github.com>
2023-07-05 01:47:31 +0900

    ⚙️ config: typescript 설치

---
+++ section1/src/index.ts
@@ -0,0 +1,8 @@
+// npm install typescript -g
+// tsc section1/src/index.ts
+// node section1/src/index.js
+// npm install ts-node -g
+// ts-node section1/src/index.ts
+
+console.log("Hello TypeScript");
+const a: number = 1;
